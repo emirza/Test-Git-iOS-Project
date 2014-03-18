998e10a881a20e683780dc31950f0be4975a3082
San Emirza <sanemirza1506@googlemail.com>
2014-03-18 17:17:12 +0100

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by San Emirza on 18/03/14.
+//  Copyright (c) 2014 San Emirza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by San Emirza on 18/03/14.
+//  Copyright (c) 2014 San Emirza. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
